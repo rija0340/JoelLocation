d70b0d42ab9999720e519c480e1feb33056f8db4
kaira <husaynmozart@gmail.com>
2021-01-03 20:59:50 +0300

    avancement

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -20,12 +20,23 @@ class AccueilController extends AbstractController
 
     
     /**
+     * @Route("/quisommenous", name="quisommenous")
+     */
+    public function quiSommeNous(): Response
+    {
+        return $this->render('accueil/quisommenous.html.twig', [
+            'controller_name' => 'QuiSommeNousController',
+        ]);
+    }
+
+    
+    /**
      * @Route("/teste", name="teste")
      */
     public function teste(): Response
     {
         return $this->render('accueil/teste.html.twig', [
-            'controller_name' => 'AccueilController',
+            'controller_name' => 'TesteController',
         ]);
     }
 }
