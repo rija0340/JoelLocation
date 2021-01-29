acf841bba0f85d3dfa1c990c231af61735eb31f0
raberia <rakotoarinelinarija@gmail.com>
2021-01-29 11:37:11 +0300

    amelioration surtous formulaire

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -4,9 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ClientType extends AbstractType
 {
@@ -19,9 +22,9 @@ class ClientType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('adresse')
-            ->add('mail')
-            ->add('telephone')
-            ->add('portable')
+            ->add('mail', EmailType::class)
+            ->add('telephone', TelType::class)
+            ->add('portable', TelType::class)
             //->add('presence')
             //->add('date_inscription')
         ;
