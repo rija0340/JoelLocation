b5965b12e2d40672f58d3a70651232fb49e85e20
kaira <husaynmozart@gmail.com>
2021-08-10 04:24:00 +0300

    ajout vehicule

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -471,7 +471,7 @@ class ClientController extends AbstractController
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($paiement);
         $entityManager->flush();
-        //return $this->redirectToRoute('espaceClient_index');
-        return $this->redirectToRoute('client');
+        return $this->redirectToRoute('espaceClient_index');
+        //return $this->redirectToRoute('client');
     }
 }
