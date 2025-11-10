832996bc5c012358a4c3f39a05b5cdebf319fbfa
Raberia <rakotoarinelinarija@gmail.com>
2025-11-10 17:59:01 +0300

    remove cdn fontawesome

--- assets/vitrine/vitrine-tailwind.js
+++ assets/vitrine/vitrine-tailwind.js
@@ -1,3 +1,6 @@
+// Import Font Awesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 // Import only Tailwind CSS for new templates
 import './css/tailwind.tailwind.css';
 // Import custom Joel colors
