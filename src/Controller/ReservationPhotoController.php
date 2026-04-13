3d11f8b9f3404e28a0a1344ca4c8f586dc40ea25
Raberia <rakotoarinelinarija@gmail.com>
2026-04-13 19:42:43 +0300

    watermark fixe size

--- src/Controller/ReservationPhotoController.php
+++ src/Controller/ReservationPhotoController.php
@@ -279,7 +279,7 @@ class ReservationPhotoController extends AbstractController
         $textColor = imagecolorallocate($image, 255, 255, 255);
 
         if ($fontFile !== null && function_exists('imagettfbbox') && function_exists('imagettftext')) {
-            $fontSize = max(28, min(120, (int) round($shortestSide * 0.05)));
+            $fontSize = 48;
             $bbox = @imagettfbbox($fontSize, 0, $fontFile, $dateText);
 
             if (is_array($bbox)) {
